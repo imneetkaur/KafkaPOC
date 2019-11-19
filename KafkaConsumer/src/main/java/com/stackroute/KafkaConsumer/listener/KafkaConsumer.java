f49871180797f3ddddef9697e13e978923b62fb5
imneetkaur <imneetkaur007@gmail.com>
2019-11-19 14:50:11 +0530

    For JSON Messages

--- KafkaConsumer/src/main/java/com/stackroute/KafkaConsumer/listener/KafkaConsumer.java
+++ KafkaConsumer/src/main/java/com/stackroute/KafkaConsumer/listener/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.stackroute.KafkaConsumer.listener;
 
+import com.stackroute.KafkaConsumer.model.User;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,10 @@ public class KafkaConsumer {
     @KafkaListener(topics = "Kafka_Example1",groupId= "group_id")
     public void consume(String message){
         System.out.println("consumed message" + message);
+    }
+
+    @KafkaListener(topics ="Kafka_Example_json",groupId ="group_json",containerFactory= "userKafkaListener")
+    public void consumeJson(User user){
+        System.out.println("consumed JSON Message" + user);
     }
 }
